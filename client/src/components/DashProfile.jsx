d449201bc7b528893bf2707418f5714637a82a21
rahul <shyamrahul815@gmail.com>
2024-03-23 21:20:18 +0530

    completed the create postpage UI

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -7,10 +7,11 @@ import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import { updateStart,updateSuccess,updateFailure,deleteUserStart,deleteUserSuccess,deleteUserFailure, signoutSuccess } from '../redux/user/userSlice'
 import {HiOutlineExclamationCircle } from 'react-icons/hi'
+import { Link } from 'react-router-dom';
 
 
 export default function DashProfile() {
-    const {currentUser, error} = useSelector(state =>state.user)
+    const {currentUser, error, loading} = useSelector(state =>state.user)
     const [imageFile, setImageFile] = useState(null)
     const [imageFileUrl, setImageFileUrl] = useState(null)
     const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -196,9 +197,20 @@ const handleSignout = async () => {
              onChange={handleChange}
             />
 
-            <Button type='submit' gradientDuoTone='purpleToBlue' outline>
-                Update
+            <Button type='submit' gradientDuoTone='purpleToBlue' outline disabled={loading || imageFileUploading}>
+               {loading ? 'Loading...' : 'Update'}
             </Button>
+        {currentUser.isAdmin && (
+          <Link to ={'/create-post'}>
+            <Button
+              type='button'
+              gradientDuoTone='purpleToPink'
+              className='w-full'
+            >
+              Create a post
+            </Button>
+          </Link>
+        )}
 
          </form>
 
