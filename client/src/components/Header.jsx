e719f19f5eb05241353dfa47436577f2ec6b7b7c
rahul <shyamrahul815@gmail.com>
2024-03-25 23:00:36 +0530

    completed search page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,20 +1,31 @@
 import { Avatar, Button, Dropdown, Navbar, NavbarToggle } from 'flowbite-react'
 import { TextInput } from 'flowbite-react'
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import {AiOutlineSearch} from 'react-icons/ai'
 import {FaMoon, FaSun} from 'react-icons/fa'
 import {useSelector, useDispatch} from 'react-redux'
 import { toggleTheme } from '../redux/theme/themeSlice'
 import { signoutSuccess } from '../redux/user/userSlice'
+import { useState, useEffect } from 'react'
 
 
 
 export default function Header() {
     const path = useLocation().pathname
+    const location = useLocation()
+    const navigate = useNavigate()
     const dispatch = useDispatch()
     const {currentUser} = useSelector((state) =>state.user) 
     const {theme} = useSelector((state) =>state.theme) 
- 
+    const [searchTerm, setSearchTerm] = useState('')
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        if (searchTermFromUrl) {
+          setSearchTerm(searchTermFromUrl);
+        }
+      }, [location.search]);
 
     const handleSignout = async () => {
         try {
@@ -31,6 +42,14 @@ export default function Header() {
           console.log(error.message);
         }
       };
+
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('searchTerm', searchTerm);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`);
+      };
     
   return (
     <Navbar className='border-b-2'>
@@ -38,12 +57,14 @@ export default function Header() {
             <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Daily's</span>
             Blog
         </Link>
-        <form>
+        <form onSubmit={handleSubmit}>
             <TextInput
                 type='text'
                 placeholder='Search'
                 rightIcon={AiOutlineSearch}
                 className='hidden lg:inline'
+                value={searchTerm}
+                onChange={(e)=>setSearchTerm(e.target.value)}
             />
         </form>
         <Button className='w-12 h-10 lg:hidden' color='gray' pill>
